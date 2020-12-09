99fa4b8950431d146bad8f9e970c3ecac7a7aac8
Christophe Leroy <christophe.leroy@c-s.fr>
2020-12-09 13:04:49 +0000

    powerpc/4xx: Remove CONFIG_4xx
    CONFIG_4xx was corresponding to CONFIG_40x | CONFIG_44x.
    
    As CONFIG_40x is gone, CONFIG_4xx and CONFIG_44x are now equivalent.
    And CONFIG_BOOKE is also set when CONFIG_44x is set.
    
    Replace (CONFIG_4xx | CONFIG_BOOKE) by CONFIG_BOOKE
    Replace other CONFIG_4xx by CONFIG_44x
    Remove CONFIG_4xx.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1477,7 +1477,7 @@ static void __show_regs(struct pt_regs *regs)
 	if (!trap_is_syscall(regs) && cpu_has_feature(CPU_FTR_CFAR))
 		pr_cont("CFAR: "REG" ", regs->orig_gpr3);
 	if (trap == 0x200 || trap == 0x300 || trap == 0x600) {
-		if (IS_ENABLED(CONFIG_4xx) || IS_ENABLED(CONFIG_BOOKE))
+		if (IS_ENABLED(CONFIG_BOOKE))
 			pr_cont("DEAR: "REG" ESR: "REG" ", regs->dar, regs->dsisr);
 		else
 			pr_cont("DAR: "REG" DSISR: %08lx ", regs->dar, regs->dsisr);
